8dd4c688a3c824f082134b73f1ffdf3085b9e418
krutulkamil <krutulkamil@onet.pl>
2023-09-17 19:38:00 +0200

    feat: sidebar logo component

--- components/sidebar/index.styles.ts
+++ components/sidebar/index.styles.ts
@@ -1,3 +1,13 @@
 import { cn } from '@/lib/utils';
 
-export const wrapperStyles = cn('space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white')
+export const wrapperStyles = cn(
+  'space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'
+);
+
+export const spacingWrapperStyles = cn('px-3 py-2 flex-1');
+
+export const logoLinkStyles = cn('flex items-center pl-3 mb-14');
+
+export const logoImageWrapperStyles = cn('relative w-8 h-8 mr-4');
+
+export const titleStyles = cn('text-2xl font-bold');
--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,13 +1,32 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Montserrat } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
 
 import * as styles from './index.styles';
+
+const montserrat = Montserrat({
+  weight: '600',
+  subsets: ['latin'],
+});
 
 export const Sidebar = () => {
   return (
     <div className={styles.wrapperStyles}>
-      Hello sidebar!
+      <div className={styles.spacingWrapperStyles}>
+        <Link href="/dashboard" className={styles.logoLinkStyles}>
+          <div className={styles.logoImageWrapperStyles}>
+            <Image src="/logo.png" alt="Genius Logo" fill />
+          </div>
+          <h1 className={cn(styles.titleStyles, montserrat.className)}>
+            Genius
+          </h1>
+        </Link>
+      </div>
     </div>
-  )
+  );
 };
