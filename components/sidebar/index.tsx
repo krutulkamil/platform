6f4fe91da7db4509ab3a9bde22f0f8b2d227e2ca
krutulkamil <krutulkamil@onet.pl>
2023-09-17 20:01:27 +0200

    refactor: routes file

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -4,17 +4,9 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Montserrat } from 'next/font/google';
-import {
-  Code,
-  ImageIcon,
-  LayoutDashboard,
-  MessageSquare,
-  Music,
-  Settings,
-  VideoIcon,
-} from 'lucide-react';
 
 import { cn } from '@/lib/utils';
+import { routes } from '@/components/sidebar/routes';
 
 import * as styles from './index.styles';
 
@@ -22,50 +14,6 @@ const montserrat = Montserrat({
   weight: '600',
   subsets: ['latin'],
 });
-
-const routes = [
-  {
-    label: 'Dashboard',
-    icon: LayoutDashboard,
-    href: '/dashboard',
-    color: 'text-sky-500',
-  },
-  {
-    label: 'Conversation',
-    icon: MessageSquare,
-    href: '/conversation',
-    color: 'text-violet-500',
-  },
-  {
-    label: 'Image Generation',
-    icon: ImageIcon,
-    href: '/image',
-    color: 'text-pink-700',
-  },
-  {
-    label: 'Video Generation',
-    icon: VideoIcon,
-    href: '/video',
-    color: 'text-orange-700',
-  },
-  {
-    label: 'Music Generation',
-    icon: Music,
-    href: '/music',
-    color: 'text-emerald-500',
-  },
-  {
-    label: 'Code Generation',
-    icon: Code,
-    href: '/code',
-    color: 'text-green-700',
-  },
-  {
-    label: 'Settings',
-    icon: Settings,
-    href: '/settings',
-  },
-];
 
 export const Sidebar = () => {
   return (
