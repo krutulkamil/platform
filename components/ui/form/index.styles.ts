b7717cfb7e0a2bb932e7032f9add21fab7f06db1
krutulkamil <krutulkamil@onet.pl>
2023-09-20 11:36:11 +0200

    feat: form & label components

---
+++ components/ui/form/index.styles.ts
@@ -0,0 +1,9 @@
+import { cn } from '@/lib/utils';
+
+export const contextSpacingStyles = cn('space-y-2');
+
+export const destructiveStyles = cn('text-destructive');
+
+export const formDescriptionStyles = cn('text-sm text-muted-foreground');
+
+export const formMessageStyles = cn('text-sm font-medium text-destructive');
---
+++ components/ui/label/index.styles.ts
@@ -0,0 +1,5 @@
+import { cn } from '@/lib/utils';
+
+export const labelBaseStyles = cn(
+  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+);
