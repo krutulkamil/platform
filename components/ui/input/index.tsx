6c529f6f187391804b9b1ab00dd3584e205e2f29
krutulkamil <krutulkamil@onet.pl>
2023-09-20 12:30:24 +0200

    feat: shadcn input component

---
+++ components/ui/input/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+import * as styles from './index.styles';
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(styles.inputBaseStyles, className)}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = 'Input';
+
+export { Input };
