f7f4310ecdc9f6f094143572500dd31699ff02c4
krutulkamil <krutulkamil@onet.pl>
2023-09-24 20:04:34 +0200

    feat: add client error components

---
+++ app/(dashboard)/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+import { Empty } from '@/components/common/empty';
+
+export default function Error() {
+  return <Empty label="Something went wrong." />;
+}
